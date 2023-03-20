8c05db5d7cde00beca5defdcac2370c23a3fdc4d
arbnorcanolli <66180576+arbnorcanolli@users.noreply.github.com>
2023-03-20 10:53:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,64 @@
 import * as React from 'react';
 
-const title = "Test2"
-const welcome = {
-  greeting:'Hi',
-  title:'Hellou'
-}
-
-function getTitle(title){
-  return title;
-}
-
 const list = [
   {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   },
   {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,},
-  ];
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-  const first = list[0];
-
-const App = () =>  (
-    <div>
-      <Search/>
-<hr />
-<List  />
-<hr />
-<Author />
-    </div>
-  );
-
-const Items = ({name}) =>{
-  return (
-    <ul>
-    {list.map(function(item){
-      return (<li key={item.objectID}>
-        <span>{item.num_comments} comments</span>
-        <br />
-        <span>Hi {name}</span>
-        <span>{item.points} points</span>
-        <br />
-      </li>
-      )
-      
-    })}
-    </ul>
-  )
-}
-
-const Search = () => {
+function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-<label htmlFor="search">Search: </label>
-<input id="search" type="text" />
+
+      <Search />
+
+      <hr />
+
+      <List />
     </div>
-  )
-}
-
-const List = () =>{
-  return (
-  <div>
-    <Items name="Arbnor" />
-    <Test />
-    <br />
-  </div>
-  )
-}
-
-const Author = () => {
-  return (
-    <ul>
-      {list.map((item) =>{
-        return (
-          <li key={item.objectID}>
-            <span>{item.author}</span>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
-
-const Test = () => {
-  return React.createElement(
-    'p',
-    {className: 'danger'},
-  'Hello React'
   );
 }
 
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
-export default App
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
